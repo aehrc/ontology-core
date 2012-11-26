4030d029f81e51a8c9681a6204ffe86c373dcd9b
Alejandro Metke <alejandro.metke@csiro.au>
2012-11-26 15:14:37 +1000

    Added equals and hashCode methods to ConceptInclusion.

--- ontology-model/src/main/java/au/csiro/ontology/axioms/ConceptInclusion.java
+++ ontology-model/src/main/java/au/csiro/ontology/axioms/ConceptInclusion.java
@@ -67,4 +67,35 @@ public class ConceptInclusion implements IConceptInclusion {
         return lhs + " \u2291 " + rhs;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((lhs == null) ? 0 : lhs.hashCode());
+        result = prime * result + ((rhs == null) ? 0 : rhs.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        ConceptInclusion other = (ConceptInclusion) obj;
+        if (lhs == null) {
+            if (other.lhs != null)
+                return false;
+        } else if (!lhs.equals(other.lhs))
+            return false;
+        if (rhs == null) {
+            if (other.rhs != null)
+                return false;
+        } else if (!rhs.equals(other.rhs))
+            return false;
+        return true;
+    }
+    
 }
