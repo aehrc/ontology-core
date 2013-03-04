c4971b4bcc26cc0c4bd77d2db612981e02fd9b6a
Alejandro Metke <alejandro.metke@csiro.au>
2013-03-04 15:23:45 +1000

    Added equals and hashCode methods to Node class.

--- ontology-model/src/main/java/au/csiro/ontology/Node.java
+++ ontology-model/src/main/java/au/csiro/ontology/Node.java
@@ -71,4 +71,39 @@ public class Node<T extends Comparable<T>> implements Serializable {
         return sb.toString();
     }
 
+    /* (non-Javadoc)
+     * @see java.lang.Object#hashCode()
+     */
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime
+                * result
+                + ((equivalentConcepts == null) ? 0 : equivalentConcepts
+                        .hashCode());
+        return result;
+    }
+
+    /* (non-Javadoc)
+     * @see java.lang.Object#equals(java.lang.Object)
+     */
+    @SuppressWarnings("rawtypes")
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Node other = (Node) obj;
+        if (equivalentConcepts == null) {
+            if (other.equivalentConcepts != null)
+                return false;
+        } else if (!equivalentConcepts.equals(other.equivalentConcepts))
+            return false;
+        return true;
+    }
+
 }
