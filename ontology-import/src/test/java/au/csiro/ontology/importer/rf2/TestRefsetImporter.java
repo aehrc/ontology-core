6a602c6b0efdc6efc04b32c86c5d74db285d11f4
Michael J Lawley <michael.lawley@csiro.au>
2013-07-15 15:40:34 +1000

    Compute the transitive closure of the module dependencies to avoid issues with buggy data

--- ontology-import/src/test/java/au/csiro/ontology/importer/rf2/TestRefsetImporter.java
+++ ontology-import/src/test/java/au/csiro/ontology/importer/rf2/TestRefsetImporter.java
@@ -33,47 +33,29 @@ public class TestRefsetImporter {
         IModuleDependencyRefset dr = 
                 RefsetImporter.importModuleDependencyRefset(refsetFiles);
         
-        Map<String, Map<String, ModuleDependency>> deps = 
-                dr.getModuleDependencies();
+        Map<String, Map<String, ModuleDependency>> deps = dr.getModuleDependencies();
         
         // Test for 32506021000036107 (20121130) -> 
         // 900000000000207008 (20120731) -> 900000000000012004 (20120731)
         ModuleDependency md = deps.get("32506021000036107").get("20121130");
         Assert.assertEquals("32506021000036107", md.getId());
         Assert.assertEquals("20121130", md.getVersion());
-        Assert.assertEquals(1, md.getDependencies().size());
+        Assert.assertEquals(2, md.getDependencies().size());
         
-        ModuleDependency md2 = md.getDependencies().iterator().next();
-        Assert.assertEquals("900000000000207008", md2.getId());
-        Assert.assertEquals("20120731", md2.getVersion());
-        Assert.assertEquals(1, md2.getDependencies().size());
-        
-        ModuleDependency md3 = md2.getDependencies().iterator().next();
-        Assert.assertEquals("900000000000012004", md3.getId());
-        Assert.assertEquals("20120731", md3.getVersion());
-        Assert.assertEquals(0, md3.getDependencies().size());
-        
-        // Test for 32570491000036106 (20120531) -> 
-        // 32506021000036107 (20120531) -> 900000000000207008 (20120131) -> 
-        // 900000000000012004 (20120131)
+        Assert.assertTrue(md.getDependencies().contains(new ModuleDependency("900000000000207008", "20120731")));
+        Assert.assertTrue(md.getDependencies().contains(new ModuleDependency("900000000000012004", "20120731")));
+
+        // Another test
         md = deps.get("32570491000036106").get("20120531");
         Assert.assertEquals("32570491000036106", md.getId());
         Assert.assertEquals("20120531", md.getVersion());
-        Assert.assertEquals(1, md.getDependencies().size());
+        Assert.assertEquals(3, md.getDependencies().size());
         
-        md2 = deps.get("32506021000036107").get("20120531");
-        Assert.assertEquals("32506021000036107", md2.getId());
-        Assert.assertEquals("20120531", md2.getVersion());
-        Assert.assertEquals(1, md2.getDependencies().size());
-        
-        md3 = md2.getDependencies().iterator().next();
-        Assert.assertEquals("900000000000207008", md3.getId());
-        Assert.assertEquals("20120131", md3.getVersion());
-        Assert.assertEquals(1, md3.getDependencies().size());
-        
-        ModuleDependency md4 = md3.getDependencies().iterator().next();
-        Assert.assertEquals("900000000000012004", md4.getId());
-        Assert.assertEquals("20120131", md4.getVersion());
-        Assert.assertEquals(0, md4.getDependencies().size());
+        Assert.assertTrue(md.getDependencies().contains(new ModuleDependency("32506021000036107", "20120531")));
+        Assert.assertTrue(md.getDependencies().contains(new ModuleDependency("900000000000207008", "20120131")));
+        Assert.assertTrue(md.getDependencies().contains(new ModuleDependency("900000000000012004", "20120131")));
+
+        // This should have no entry
+        Assert.assertNull(deps.get("900000000000012004"));
     }
 }
