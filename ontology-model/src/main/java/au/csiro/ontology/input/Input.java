abee3e485acc270a41c135baee4bf6528f2ea45f
Michael J Lawley <michael.lawley@csiro.au>
2013-10-07 11:59:07 -0400

    explicit check for missing classpath-relative resources

--- ontology-model/src/main/java/au/csiro/ontology/input/Input.java
+++ ontology-model/src/main/java/au/csiro/ontology/input/Input.java
@@ -2,6 +2,7 @@ package au.csiro.ontology.input;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.zip.ZipFile;
@@ -9,27 +10,27 @@ import java.util.zip.ZipFile;
 
 /**
  * Base class for inputs.
- * 
+ *
  * @author Alejandro Metke
  */
 public abstract class Input {
-    
+
     /**
      * Indicates if the files should be loaded from an external file system or
      * the class path.
-     * 
+     *
      * @author Alejandro Metke
      *
      */
     public enum InputType {
         EXTERNAL, CLASSPATH
     };
-    
+
     protected InputType inputType;
     protected String base;
 
     protected ZipFile zip = null;
-    
+
     /**
      * @return the type
      */
@@ -60,7 +61,7 @@ public abstract class Input {
 
     public InputStream getInputStream(String name) throws IOException {
         final InputType type = getInputType();
-        
+
         if (type.equals(InputType.EXTERNAL)) {
             File file;
             if (base != null) {
@@ -79,15 +80,19 @@ public abstract class Input {
             } else {
                 file = new File(name);                          // No base; default name resolution
             }
-            
+
             return new FileInputStream(file);
         } else if (type.equals(InputType.CLASSPATH)) {
-            return this.getClass().getResourceAsStream(name);
+            final InputStream stream = this.getClass().getResourceAsStream(name);
+            if (null == stream) {
+                throw new FileNotFoundException(name + " not found in classpath");
+            }
+            return stream;
         } else {
             throw new RuntimeException("Unexpected input type " + type);
         }
     }
-    
+
     /**
      * Release internal state.
      * <p>
@@ -99,5 +104,5 @@ public abstract class Input {
             zip = null;
         }
     }
-    
+
 }
