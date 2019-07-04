dc2a7451fbbf92b378e66d285224456514604b54
Michael Lawley <michael.lawley@csiro.au>
2019-07-04 18:34:25 +1000

    use newer flat OWL axioms for concrete domains

--- ontology-import/src/test/java/au/csiro/ontology/importer/rf2/TestRF2Importer.java
+++ ontology-import/src/test/java/au/csiro/ontology/importer/rf2/TestRF2Importer.java
@@ -4,14 +4,46 @@
  */
 package au.csiro.ontology.importer.rf2;
 
+import java.util.Collections;
+import java.util.Iterator;
 
+import org.junit.Test;
+
+import au.csiro.ontology.Ontology;
+import au.csiro.ontology.input.Input.InputType;
+import au.csiro.ontology.input.Inputs.ReleaseType;
+import au.csiro.ontology.input.RF2Input;
+import junit.framework.Assert;
 
 /**
  * Unit tests for {@link RF2Importer}.
- * 
- * @author Alejandro Metke
- * 
+ *
+ * @author Michael Lawley
+ *
  */
 public class TestRF2Importer {
+
+    @Test
+    public void testExtractVersionRows() {
+        try {
+            final RF2Input input = new RF2Input();
+            input.setInputType(InputType.CLASSPATH);
+            input.setReleaseType(ReleaseType.FULL);
+            input.setModuleDependenciesRefsetFiles(Collections.singleton("/der2_ssRefset_ModuleDependencyFull_AU1000036_20121130.txt"));
+            input.setConceptsFiles(Collections.singleton("/rf2_full_con_test.txt"));
+            input.setRelationshipsFiles(Collections.singleton("/rf2_full_rel_test.txt"));
+
+            final RF2Importer rf2i = new RF2Importer(input);
+            final Iterator<Ontology> itr = rf2i.getOntologyVersions(null);
+            while (itr.hasNext()) {
+                Ontology o = itr.next();
+                System.err.println(o);
+            }
+//                    "20110731");
+        } catch(Exception e) {
+            e.printStackTrace();
+            Assert.assertTrue(false);
+        }
+    }
 
 }
